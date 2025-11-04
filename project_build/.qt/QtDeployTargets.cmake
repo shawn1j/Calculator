fc38e4ce38c916b2a5d8fcdba21e2d8195ad7a77
Shanw <pandora3651@outlook.com>
2025-11-04 11:16:00 +0800

    移除了编译产生的中间文件

--- project_build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_calculator_FILE E:/qt_project/calculator/project_build/calculator.exe)
-set(__QT_DEPLOY_TARGET_calculator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_calculator_RUNTIME_DLLS D:/Qt/6.8.3/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.8.3/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.8.3/msvc2022_64/bin/Qt6Cored.dll)
